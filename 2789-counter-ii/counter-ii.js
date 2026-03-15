702327b1c6ed6dcdf45d97b5bf69f05d26d2f1e6
AhmedEssamECE <manazhgt@gmail.com>
2026-03-15 05:53:54 +0200

    Time: 46 ms (77.29%) | Memory: 56 MB (89.69%) - LeetSync

---
+++ 2789-counter-ii/counter-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let presentCount = init;
+    return {
+        increment:function(){
+            return ++presentCount ;
+        }
+        ,
+        reset:function(){
+          return( presentCount = init);
+        },
+        decrement:function(){
+
+         return --presentCount;
+        }
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
