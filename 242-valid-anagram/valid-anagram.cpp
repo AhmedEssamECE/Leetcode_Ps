c8b7bc4a74c9b7ba2382bf094ee00c1b13baf9c3
AhmedEssamECE <manazhgt@gmail.com>
2026-07-12 19:44:57 +0300

    Time: 4 ms (34.91%) | Memory: 9.9 MB (25.80%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length())
+        {
+            return false;
+        }
+      map<char, int> counts;
+     for(int i =0 ; i<s.length() ; i++)
+     {
+        counts[s[i]]++;
+        counts[t[i]]--;
+
+     }
+     for(auto count : counts)
+       if(count.second)
+       {
+        return false;
+       }
+       return true;
+        
+    }
+};
